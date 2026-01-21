98c79759998ae4e60df4c574f193f776116b8e77
manan226927-art <manan226927@gmail.com>
2026-01-22 01:44:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect
 
 app = Flask(__name__)
 
@@ -34,3 +34,4 @@ def bank():
 if __name__ == "__main__":
 
     app.run(debug=True)
+
